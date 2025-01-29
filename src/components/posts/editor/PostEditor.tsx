4b647fb4d08e1684a682cb8ab77229a16616efaf
inuldev <inuldev0@gmail.com>
2025-01-29 19:17:36 +0700

    react query cache mutation

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -4,15 +4,17 @@ import StarterKit from "@tiptap/starter-kit";
 import Placeholder from "@tiptap/extension-placeholder";
 import { EditorContent, useEditor } from "@tiptap/react";
 
-import { Button } from "@/components/ui/button";
 import UserAvatar from "@/components/UserAvatar";
+import LoadingButton from "@/components/LoadingButton";
 import { useSession } from "@/app/(main)/SessionProvider";
 
 import "./styles.css";
-import { submitPost } from "./actions";
+import { useSubmitPostMutation } from "./mutations";
 
 export default function PostEditor() {
   const { user } = useSession();
+
+  const mutation = useSubmitPostMutation();
 
   const editor = useEditor({
     extensions: [
@@ -32,9 +34,12 @@ export default function PostEditor() {
       blockSeparator: "\n",
     }) || "";
 
-  async function onSubmit() {
-    await submitPost(input);
-    editor?.commands.clearContent();
+  function onSubmit() {
+    mutation.mutate(input, {
+      onSuccess: () => {
+        editor?.commands.clearContent();
+      },
+    });
   }
 
   return (
@@ -47,13 +52,14 @@ export default function PostEditor() {
         />
       </div>
       <div className="flex justify-end">
-        <Button
+        <LoadingButton
           onClick={onSubmit}
+          loading={mutation.isPending}
           disabled={!input.trim()}
           className="min-w-20"
         >
           Post
-        </Button>
+        </LoadingButton>
       </div>
     </div>
   );
