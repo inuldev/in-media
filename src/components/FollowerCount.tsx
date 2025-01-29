663d72be71cf74bd94c187b36a67384bf8b40df4
inuldev <inuldev0@gmail.com>
2025-01-29 23:45:35 +0700

    user profile page

---
+++ src/components/FollowerCount.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { formatNumber } from "@/lib/utils";
+import { FollowerInfo } from "@/lib/types";
+import useFollowerInfo from "@/hooks/useFollowerInfo";
+
+interface FollowerCountProps {
+  userId: string;
+  initialState: FollowerInfo;
+}
+
+export default function FollowerCount({
+  userId,
+  initialState,
+}: FollowerCountProps) {
+  const { data } = useFollowerInfo(userId, initialState);
+
+  return (
+    <span>
+      Followers:{" "}
+      <span className="font-semibold">{formatNumber(data.followers)}</span>
+    </span>
+  );
+}
