6caba36a2d0e3f3b9028e39144fecac672332514
inuldev <inuldev0@gmail.com>
2025-02-01 01:42:37 +0700

    notifications feature

--- src/app/(main)/MenuBar.tsx
+++ src/app/(main)/MenuBar.tsx
@@ -1,13 +1,28 @@
 import Link from "next/link";
-import { Bell, Bookmark, Home, Mail } from "lucide-react";
+import { Bookmark, Home, Mail } from "lucide-react";
+
+import { validateRequest } from "@/auth";
+import { prisma } from "@/lib/prisma";
 
 import { Button } from "@/components/ui/button";
+import NotificationsButton from "./NotificationsButton";
 
 interface MenuBarProps {
   className?: string;
 }
 
-export default function MenuBar({ className }: MenuBarProps) {
+export default async function MenuBar({ className }: MenuBarProps) {
+  const { user } = await validateRequest();
+
+  if (!user) return null;
+
+  const unreadNotificationCount = await prisma.notification.count({
+    where: {
+      recipientId: user.id,
+      read: false,
+    },
+  });
+
   return (
     <div className={className}>
       <Button
@@ -21,17 +36,9 @@ export default function MenuBar({ className }: MenuBarProps) {
           <span className="hidden lg:inline">Home</span>
         </Link>
       </Button>
-      <Button
-        variant="ghost"
-        className="flex items-center justify-start gap-3"
-        title="Notifications"
-        asChild
-      >
-        <Link href="/notifications">
-          <Bell />
-          <span className="hidden lg:inline">Notifications</span>
-        </Link>
-      </Button>
+      <NotificationsButton
+        initialState={{ unreadCount: unreadNotificationCount }}
+      />
       <Button
         variant="ghost"
         className="flex items-center justify-start gap-3"
